b5a0b19a7f75d732bfeeb0dcc9bd335dd98e7fc1
Tamhm <hoangtam2652@gmail.com>
2023-06-23 10:13:28 +0700

    3 tiles

---
+++ Assets/data/Grid/GridSystem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem : SaiMonoBehaviour
+{
+    public List<note> nodes;
+    public int width = 20;
+    public int height = 13;
+
+} 
+
---
+++ Assets/data/Grid/note.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class note
+{
+    public int x = 0;
+    public int y = 0;
+    public int weight = 1;
+    public bool occupied = false;
+    public note up;
+    public note down;
+    public note left;
+    public note right;
+}
+
